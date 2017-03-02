6bc700d63a87b87a1b82e8ca53549ebd04eb7a6b
Yulia Gaponenko <yulia.gaponenko1@de.ibm.com>
2017-03-02 11:33:04 +0100

    Update for ppc64le

--- cf_spec/unit/helpers/node_installer_spec.rb
+++ cf_spec/unit/helpers/node_installer_spec.rb
@@ -12,18 +12,28 @@ describe LanguagePack::NodeInstaller do
       end
 
       it 'always executes the modern Fetcher' do
-        node_tar_regex = Regexp.new('node\-v\d+\.\d+\.\d+' + Regexp.quote('-linux-x64.tar.gz'))
-        extract_path_regex = Regexp.new('node\-v\d+\.\d+\.\d+' + Regexp.quote('-linux-x64/bin/node'))
+        node_tar_regex = Regexp.new('node\-v\d+\.\d+\.\d+' + Regexp.quote("-linux-{platform}.tar.gz"))
+        extract_path_regex = Regexp.new('node\-v\d+\.\d+\.\d+' + Regexp.quote("-linux-{platform}/bin/node"))
         expect_any_instance_of(LanguagePack::Fetcher).to receive(:fetch_untar).
           with(node_tar_regex, extract_path_regex)
         installer.install
       end
 
       it 'moves the node binary to the current path' do
-        extract_path_regex = Regexp.new('node\-v\d+\.\d+\.\d+' + Regexp.quote('-linux-x64/bin/node'))
+        extract_path_regex = Regexp.new('node\-v\d+\.\d+\.\d+' + Regexp.quote("-linux-{platform}/bin/node"))
         expect(FileUtils).to receive(:mv).with(extract_path_regex, '.')
         installer.install
       end
     end
+  end
+
+  def platform
+    platform_map = { 'x86_64' => 'x64',
+                     'powerpc64le' => 'ppc64le'}
+    platform_map[arch]
+  end
+
+  def arch
+    RbConfig::CONFIG['host_cpu']
   end
 end
--- lib/cloud_foundry/language_pack/helpers/node_installer.rb
+++ lib/cloud_foundry/language_pack/helpers/node_installer.rb
@@ -1,4 +1,10 @@
 class LanguagePack::NodeInstaller
+
+  arch = RbConfig::CONFIG['host_cpu']
+  platform_map = { 'x86_64' => 'x64',
+                   'powerpc64le' => 'ppc64le'}
+  platform = platform_map[arch]
+
   # Get default node version from manifest
   bin_path = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "..", "compile-extensions", "bin"))
   manifest_path = File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "..", "..", "manifest.yml"))
@@ -9,7 +15,12 @@ class LanguagePack::NodeInstaller
   self.send(:remove_const, const) if self.const_defined?(const)
   MODERN_NODE_VERSION = default_node_version
 
+  const = 'PLATFORM_VERSION'
+  self.send(:remove_const, const) if self.const_defined?(const)
+  PLATFORM_VERSION = platform
+
   const = 'MODERN_BINARY_PATH'
   self.send(:remove_const, const) if self.const_defined?(const)
-  MODERN_BINARY_PATH = "node-v#{MODERN_NODE_VERSION}-linux-x64"
+
+  MODERN_BINARY_PATH = "node-v#{MODERN_NODE_VERSION}-linux-#{PLATFORM_VERSION}"
 end
